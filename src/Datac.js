47de0efb3fcef433a60be95e26041d7f4720dd37
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-03-26 03:16:37 +0530

    Update Datac.js

--- src/Datac.js
+++ src/Datac.js
@@ -7,21 +7,19 @@ const Datac = () => {
     <section className='container'>
       <ul className='users'>
         {participants.map((participant) => {
-          const { id, name, tasks, email, photo } = participant
-          const progress = (tasks / 5) * 100
-          console.log(participant)
+          const { id, name, tasks, email, image } = participant
+          // console.log(participant)
           return (
             <li key={id}>
-              <img src={photo}></img>
+              <img src={image}></img>
               <div className='name'>
                 <h1>{name}</h1>
               </div>
               <div className='email'>
                 <h2>{email}</h2>
               </div>
-
               <div className='progress'>
-                <div className='progress-bar'></div>
+                <h1>{tasks}/5</h1>
               </div>
             </li>
           )
