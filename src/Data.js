2cffebf1af0f3291199260584997439a9c2fe757
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-03-26 03:16:55 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -4,41 +4,44 @@ export const participants = [
     name: 'Sahil Arora',
     tasks: 5,
     email: 'Sahil@github/',
-    img: 'https://e1.pxfuel.com/desktop-wallpaper/276/567/desktop-wallpaper-anime-death-note-light-yagami-mobile.jpg',
+    image:
+      'https://lh3.googleusercontent.com/ogw/AAEL6sgIGhKYbxIlW9MJwPdrEgOGWMGsoitanVkWi593Yg=s32-c-mo',
   },
   {
     id: 2,
     name: 'Kira Yagami',
     tasks: 4,
     email: 'Light@github/',
-    img: 'https://e1.pxfuel.com/desktop-wallpaper/276/567/desktop-wallpaper-anime-death-note-light-yagami-mobile.jpg',
+    image:
+      'https://e1.pxfuel.com/desktop-wallpaper/276/567/desktop-wallpaper-anime-death-note-light-yagami-mobile.jpg',
   },
   {
     id: 3,
     name: 'Zero Geass',
     tasks: 3,
     email: 'Code.1@github/',
-    img: 'https://e1.pxfuel.com/desktop-wallpaper/276/567/desktop-wallpaper-anime-death-note-light-yagami-mobile.jpg',
+    image: 'https://cdn.myanimelist.net/images/characters/8/406163.jpg',
   },
   {
     id: 4,
     name: 'Futaba Rio',
     tasks: 4,
     email: 'Futaba.hello@github/',
-    img: 'https://w7.pngwing.com/pngs/452/495/png-transparent-react-javascript-angularjs-ionic-github-text-logo-symmetry.png',
+    image: 'https://images7.alphacoders.com/970/thumbbig-970733.webp',
   },
   {
     id: 5,
     name: 'Mai-senpai',
     tasks: 1,
     email: 'Bunny.girl@github/',
-    img: 'https://wallpapercave.com/wp/wp4055492.jpg',
+    image: 'https://wallpapercave.com/wp/wp4055492.jpg',
   },
   {
     id: 6,
     name: 'Izuku Midoriya',
     tasks: 2,
     email: 'Deku-onrforall@github/',
-    img: 'https://e1.pxfuel.com/desktop-wallpaper/276/567/desktop-wallpaper-anime-death-note-light-yagami-mobile.jpg',
+    image:
+      'https://www.fredzone.org/wp-content/uploads/2023/02/anime-my-hero-academia-saison-6-opening-850x560.jpeg',
   },
 ]
